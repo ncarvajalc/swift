199df12684dcf11074f54bb3d2e5c16590f85a9a
ncarvajalc <n.carvajalc@uniandes.edu.co>
2020-06-09 12:44:26 -0500

    Initial Commit

---
+++ PruebaGot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PruebaGot
+//
+//  Created by Nicolás Carvajal on 9/06/20.
+//  Copyright © 2020 Nicolás Carvajal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
